31f79ffd6c5b33b735c6abdbc2d3696af9b72ea5
Nicolás Courtalón <courtalonnicolasm@gmail.com>
2024-08-31 17:01:39 -0300

    Final Version 1.0

--- src/main/java/nc/diedtp/Main.java
+++ src/main/java/nc/diedtp/Main.java
@@ -23,20 +23,29 @@ public class Main {
       clientes.add(new Cliente("pablo",58487,"pablo.perez@hotmail.com","Calchines 1562",0.0,0.0));
       clientes.add(new Cliente("segundo",45872,"segundo@gmail.com","Francia 2022",0.0,0.0));
       
-      vendedores.add(new Vendedor("Jeremias", "Belgrano 962d4"));
-      vendedores.add(new Vendedor("Luis", "Tucuman 8080"));
-      vendedores.add(new Vendedor("Juan", "San Jeronimo 2654"));
-     
-      Coordenada c51 = new Coordenada(-31.6365133,-60.703816);
-      Coordenada c52 = new Coordenada(-31.6171694, -60.6780453);
-      System.out.println(c51);
-      System.out.println(c52);
-      System.out.println(c51.distance(c52));
+      vendedores.add(new Vendedor("Jeremias", "Belgrano 9624", 4.0, 5.2));
+      vendedores.add(new Vendedor("Luis", "Tucuman 8080", 2.1,-4.0));
+      vendedores.add(new Vendedor("Juan", "San Jeronimo 2654",1.0,-1.12));
+      Scanner entrada = new Scanner(System.in);
+      listarVendedores(vendedores);
+      Vendedor v_encontrado = buscarVendedor(vendedores, entrada);
+      if (v_encontrado != null) {
+          System.out.println("Vendedor encontrado: " + v_encontrado.getNombre());
+      } else {
+          System.out.println("Vendedor no encontrado.");
+      }
+      v_encontrado = null;
+      v_encontrado = eliminarVendedor(vendedores, entrada);
       
-      Cliente cliente = new Cliente();
-        
+      if (v_encontrado != null) {
+           System.out.println("Vendedor eliminado: " + v_encontrado.getNombre());
+       } else {
+           System.out.println("Vendedor no encontrado ni eliminado.");
+       }
+      listarVendedores(vendedores);
+      listarClientes(clientes);
       // Llamar al método buscarCliente
-      Cliente encontrado = buscarCliente(clientes);
+      Cliente encontrado = buscarCliente(clientes, entrada);
       
       // Verificar si se encontró el cliente
       if (encontrado != null) {
@@ -46,7 +55,9 @@ public class Main {
       }
       
        // Llamar al método buscarCliente
-       Cliente c1 = eliminarCliente(clientes);
+       Cliente c1 = eliminarCliente(clientes, entrada);
+       
+       
       
        // Verificar si se encontró el cliente
        if (c1 != null) {
@@ -54,12 +65,12 @@ public class Main {
        } else {
            System.out.println("Cliente no encontrado ni eliminado.");
        }
+       entrada.close();
        listarClientes(clientes);
     }
-    public static Cliente buscarCliente(ArrayList<Cliente> clientes) {
+    public static Cliente buscarCliente(ArrayList<Cliente> clientes, Scanner entrada) {
         String clie;
         System.out.println("Ingrese el nombre o id del cliente que desea buscar");
-        Scanner entrada = new Scanner(System.in);
         clie = entrada.nextLine();
         for (Cliente cliente : clientes) {
             if (cliente.getNombre().equals(clie)) {
@@ -73,7 +84,6 @@ public class Main {
                 // Ignorar la excepcion 
             }
         }
-        entrada.close();
         return null;
     }
     public static void listarClientes(ArrayList<Cliente> clientes){
@@ -82,10 +92,15 @@ public class Main {
             System.out.println(cliente);
         }
     }
-    public static Cliente eliminarCliente(ArrayList<Cliente> clientes){
+    public static void listarVendedores(ArrayList<Vendedor> vendedores){
+        System.out.println("Lista de Vendedores:");
+        for(Vendedor vendedor : vendedores){
+            System.out.println(vendedor);
+        }
+    }
+    public static Cliente eliminarCliente(ArrayList<Cliente> clientes, Scanner entrada){
         String clie;
         System.out.println("Ingrese el nombre o id del cliente que desea eliminar");
-        Scanner entrada = new Scanner(System.in);
         clie = entrada.nextLine();
         for (Cliente cliente : clientes) {
             if (cliente.getNombre().equals(clie)) {
@@ -101,7 +116,44 @@ public class Main {
                 // Ignorar la excepccion. esta se puede dar por el mismo motivo q en la busqueda
             }
         }
-        entrada.close();
+        return null;
+    }
+    public static Vendedor eliminarVendedor(ArrayList<Vendedor> vendedores, Scanner entrada){
+        String clie;
+        System.out.println("Ingrese el nombre o id del vendedor que desea eliminar");
+        clie = entrada.nextLine();
+        for (Vendedor vendedor : vendedores) {
+            if (vendedor.getNombre().equals(clie)) {
+                vendedores.remove(vendedor);
+                return vendedor;
+            }
+            try {
+                if (vendedor.getId() == Integer.parseInt(clie)) {
+                    vendedores.remove(vendedor);
+                    return vendedor;
+                }
+            } catch (NumberFormatException e) {
+                // Ignorar la excepccion. esta se puede dar por el mismo motivo q en la busqueda
+            }
+        }
+        return null;
+    }
+    public static Vendedor buscarVendedor(ArrayList<Vendedor> vendedores, Scanner entrada) {
+        String vend;
+        System.out.println("Ingrese el nombre o id del vendedor que desea buscar");
+        vend = entrada.nextLine();
+        for (Vendedor vendedor : vendedores) {
+            if (vendedor.getNombre().equals(vend)) {
+                return vendedor;
+            }
+            try {
+                if (vendedor.getId() == Integer.parseInt(vend)) {
+                    return vendedor;
+                }
+            } catch (NumberFormatException e) {
+                // Ignorar la excepcion 
+            }
+        }
         return null;
     }
 }
--- src/main/java/nc/diedtp/Vendedor.java
+++ src/main/java/nc/diedtp/Vendedor.java
@@ -40,23 +40,18 @@ public class Vendedor {
    {
        return nombre;
    }
+    @Override
+    public String toString(){
+        return "Nombre: "+nombre+"\nId: "+Integer.toString(id)+"\nDireccion: "+direccion;
+    }
 
    private static final double R = 6371; // radio de la tierra en km
 
     public double distancia (Cliente cliente) {
-        // convertir de grados a radianes
-        // double lat1 = coordenada.lat;
-        // double lon1 = coordenada.lng;
-        // double lat2 = c.getCoordenada().lat;
-        // double lon2 = c.getCoordenada().lng;
-        double lat1Rad = Math.toRadians(this.getCoordenada().lat);
-        double lon1Rad = Math.toRadians(this.getCoordenada().lng);
-        double lat2Rad = Math.toRadians(cliente.getCoordenada().lat);
-        double lon2Rad = Math.toRadians(cliente.getCoordenada().lng);
-        // double lat1Rad = Math.toRadians(lat1);
-        // double lon1Rad = Math.toRadians(lon1);
-        // double lat2Rad = Math.toRadians(lat2);
-        // double lon2Rad = Math.toRadians(lon2);
+        double lat1Rad = Math.toRadians(this.getCoordenada().getLatitude());
+        double lon1Rad = Math.toRadians(this.getCoordenada().getLongitude());
+        double lat2Rad = Math.toRadians(cliente.getCoordenada().getLatitude());
+        double lon2Rad = Math.toRadians(cliente.getCoordenada().getLongitude());
 
         // lat2 - lat1
         double dLat = lat2Rad - lat1Rad;
@@ -71,16 +66,4 @@ public class Vendedor {
         // devuelve la distnacia en km entre los dos puntos
         return R * c;
     }
-   public Vendedor buscarVendedor(ArrayList<Vendedor> vendedores){
-        String vend;
-        Scanner entrada = new Scanner (System.in);
-      vend = entrada.next(); 
-      for(int i=0; i<vendedores.size();i++){
-           if (vendedores.get(i).nombre.equals(vend) || vendedores.get(i).getId() == Integer.parseInt(vend)){
-                return vendedores.get(i);
-          }
-        }
-      entrada.close();
-      return null;
-      }
 }
