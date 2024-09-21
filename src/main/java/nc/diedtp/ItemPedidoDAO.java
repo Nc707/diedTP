1be3ec4fa3aac0f263a034e6e47855b0aad88890
Juan Cruz Kraft <kraft.juancruz@gmail.com>
2024-09-21 14:34:45 -0300

    excepciones parte2

--- src/main/java/nc/diedtp/ItemPedidoDAO.java
+++ src/main/java/nc/diedtp/ItemPedidoDAO.java
@@ -1,15 +1,17 @@
-
 package nc.diedtp;
 
 import java.util.ArrayList;
-
-import nc.diedtp.excepciones.ItemNoEncontradoException;
-
+import nc.diedtp.excepciones.*;
 
 public interface ItemPedidoDAO {
+
     ArrayList<ItemPedido> busquedaPorRango(float piso, float tope) throws ItemNoEncontradoException;
+
     ArrayList<ItemPedido> buscarPorVendedor(int id) throws ItemNoEncontradoException;
+
     ArrayList<ItemPedido> filtrarCliente(int id) throws ItemNoEncontradoException;
+
     void filtrarCategoria(); // no se implemento;
-    ArrayList<ItemPedido> OrdenarPorCantidadASC (int idPedido);
+
+    ArrayList<ItemPedido> OrdenarPorCantidadASC(int idPedido) throws PedidoNoEncontradoException;
 }
--- src/main/java/nc/diedtp/ItemPedidoMemory.java
+++ src/main/java/nc/diedtp/ItemPedidoMemory.java
@@ -1,13 +1,13 @@
 package nc.diedtp;
 
 import java.util.ArrayList;
+import nc.diedtp.excepciones.*;
 
-import nc.diedtp.excepciones.ItemNoEncontradoException;
+public class ItemPedidoMemory implements ItemPedidoDAO {
 
-public class ItemPedidoMemory implements ItemPedidoDAO{
-    private ArrayList <Pedido> listaPedidos;
-    
- public ItemPedidoMemory(ArrayList<Pedido> listaPedido) {
+    private ArrayList<Pedido> listaPedidos;
+
+    public ItemPedidoMemory(ArrayList<Pedido> listaPedido) {
         this.listaPedidos = listaPedido;
     }
 
@@ -18,142 +18,137 @@ public class ItemPedidoMemory implements ItemPedidoDAO{
     public void setListaItemsPedido(ArrayList<Pedido> listaPedido) {
         this.listaPedidos = listaPedido;
     }
- 
-    public void addItemPedido(Pedido pedido){
+
+    public void addItemPedido(Pedido pedido) {
         this.listaPedidos.add(pedido);
-        
+
     }
 
-    @Override 
+    @Override
     public ArrayList<ItemPedido> busquedaPorRango(float piso, float tope) throws ItemNoEncontradoException {
-    ArrayList<ItemPedido> aux = new ArrayList<>();
-    String cant, nombreItemMenu, precio;
-    
-    for (Pedido ped : listaPedidos) {
-        for (ItemPedido item : ped.getItemsPedido()) {
-            if ((item.getItemMenu().getPrecio() >= piso) && (item.getItemMenu().getPrecio() <= tope)) {
-                aux.add(item);
-                cant = String.valueOf(item.getCantidad());
-                nombreItemMenu = item.getItemMenu().getNombre();
-                precio = String.valueOf(item.getItemMenu().getPrecio());
-                System.out.println(cant + " " + nombreItemMenu + " " + precio);
+        ArrayList<ItemPedido> aux = new ArrayList<>();
+        String cant, nombreItemMenu, precio;
+
+        for (Pedido ped : listaPedidos) {
+            for (ItemPedido item : ped.getItemsPedido()) {
+                if ((item.getItemMenu().getPrecio() >= piso) && (item.getItemMenu().getPrecio() <= tope)) {
+                    aux.add(item);
+                    cant = String.valueOf(item.getCantidad());
+                    nombreItemMenu = item.getItemMenu().getNombre();
+                    precio = String.valueOf(item.getItemMenu().getPrecio());
+                    System.out.println(cant + " " + nombreItemMenu + " " + precio);
+                }
             }
         }
-    }
-    
-    if (aux.isEmpty()) {
-        throw new ItemNoEncontradoException("No se encontraron ítems en el rango de precios: " + piso + " a " + tope);
-    }
-    
-    return aux;
-}
 
-    
-    
+        if (aux.isEmpty()) {
+            throw new ItemNoEncontradoException("No se encontraron ítems en el rango de precios: " + piso + " a " + tope);
+        }
+
+        return aux;
+    }
+
     @Override
     public ArrayList<ItemPedido> buscarPorVendedor(int idVendedor) throws ItemNoEncontradoException {
-    ArrayList<ItemPedido> aux = new ArrayList<>();
-    for (Pedido pedido : listaPedidos) {
-        for (ItemPedido item : pedido.getItemsPedido()) {
-            if (item.getItemMenu().getVendedor().getId() == idVendedor) {
-                aux.add(item);
+        ArrayList<ItemPedido> aux = new ArrayList<>();
+        for (Pedido pedido : listaPedidos) {
+            for (ItemPedido item : pedido.getItemsPedido()) {
+                if (item.getItemMenu().getVendedor().getId() == idVendedor) {
+                    aux.add(item);
+                }
             }
         }
+
+        if (aux.isEmpty()) {
+            throw new ItemNoEncontradoException("No se encontraron ítems para el vendedor con ID: " + idVendedor);
+        }
+
+        return aux;
     }
-    
-    if (aux.isEmpty()) {
-        throw new ItemNoEncontradoException("No se encontraron ítems para el vendedor con ID: " + idVendedor);
-    }
-    
-    return aux;
-}
 
     @Override
-    public ArrayList<ItemPedido> filtrarCliente(int id) throws ItemNoEncontradoException { 
+    public ArrayList<ItemPedido> filtrarCliente(int id) throws ItemNoEncontradoException {
         // semanticamente no deberia lanzar esta excepcion porque no es que no encuentra items, sino que no encuentra el cliente
-    ArrayList<ItemPedido> aux = new ArrayList<>();
-    Cliente cliente = null;
+        ArrayList<ItemPedido> aux = new ArrayList<>();
+        Cliente cliente = null;
 
-    for (Pedido ped : listaPedidos) {
-        if (id == ped.getCliente().getId()) {
-            cliente = ped.getCliente();
-            break;
+        for (Pedido ped : listaPedidos) {
+            if (id == ped.getCliente().getId()) {
+                cliente = ped.getCliente();
+                break;
+            }
         }
-    }
 
-    // Lanza excepcin si no se encuentra el cliente
-    if (cliente == null) {
-        throw new ItemNoEncontradoException("No se encontró un cliente con el ID: " + id);
-    }
-
-    System.out.println("Items pedidos por " + cliente.getNombre() + ":");
-    
-    for (Pedido ped : listaPedidos) {
-        if (cliente.equals(ped.getCliente())) {
-            aux.addAll(ped.getItemsPedido());
-            ped.mostrarItems();
+        // Lanza excepcin si no se encuentra el cliente
+        if (cliente == null) {
+            throw new ItemNoEncontradoException("No se encontró un cliente con el ID: " + id);
         }
+
+        System.out.println("Items pedidos por " + cliente.getNombre() + ":");
+
+        for (Pedido ped : listaPedidos) {
+            if (cliente.equals(ped.getCliente())) {
+                aux.addAll(ped.getItemsPedido());
+                ped.mostrarItems();
+            }
+        }
+
+        return aux;
     }
 
-    return aux;
-}
-
-
-    
-  
     @Override
-    public void filtrarCategoria(){
-        
+    public void filtrarCategoria() {
+
     }
-    
-    
+
     @Override
-    public ArrayList<ItemPedido> OrdenarPorCantidadASC (int idPedido){
-        ArrayList<ItemPedido> aux =new ArrayList();
-        for(Pedido pedido : listaPedidos){
-        if( pedido.getId()== idPedido){
-            aux = pedido.getItemsPedido();
-            break;
+    public ArrayList<ItemPedido> OrdenarPorCantidadASC(int idPedido) throws PedidoNoEncontradoException {
+        ArrayList<ItemPedido> aux = null;
+        for (Pedido pedido : listaPedidos) {
+            if (pedido.getId() == idPedido) {
+                aux = pedido.getItemsPedido();
+                break;
+            }
         }
+        if (aux == null) {
+            throw new PedidoNoEncontradoException("No se encontró un pedido con el ID: " + idPedido);
         }
-        
-          return mergeSort(aux);
+        return mergeSort(aux);
     }
 
     private ArrayList<ItemPedido> mergeSort(ArrayList<ItemPedido> aux) {
-        if( aux.size() <=1){
+        if (aux.size() <= 1) {
             return aux;
         }
-        
-        int medio= aux.size()/2;
-        ArrayList<ItemPedido> izq= new ArrayList<>(aux.subList(0, medio));
-        ArrayList<ItemPedido> der= new ArrayList<>(aux.subList(medio, aux.size()));
-       return merge(mergeSort(izq), mergeSort(der));        
-    }
-   
-    private ArrayList<ItemPedido> merge(ArrayList<ItemPedido> izq, ArrayList<ItemPedido> der) {
-        int i = 0, j =0; //i recorre el izq y j recorre el der;
-        ArrayList<ItemPedido> resultado= new ArrayList<>();
-        while(i < izq.size() && j< der.size()){
-                if(izq.get(i).getCantidad() <= der.get(j).getCantidad()){
-                    resultado.add(izq.get(i));
-                    i++;
-                }else {
-                    resultado.add(der.get(j));
-                    j++;
-                    }
-                }
-        while (i <izq.size()) {
-        resultado.add(izq.get(i));
-        i++;
-    }
-    
-    while (j<der.size()) {
-        resultado.add(der.get(j));
-        j++;
-    }
-    
-    return resultado;
-}
+
+        int medio = aux.size() / 2;
+        ArrayList<ItemPedido> izq = new ArrayList<>(aux.subList(0, medio));
+        ArrayList<ItemPedido> der = new ArrayList<>(aux.subList(medio, aux.size()));
+        return merge(mergeSort(izq), mergeSort(der));
     }
 
+    private ArrayList<ItemPedido> merge(ArrayList<ItemPedido> izq, ArrayList<ItemPedido> der) {
+        int i = 0, j = 0; //i recorre el izq y j recorre el der;
+        ArrayList<ItemPedido> resultado = new ArrayList<>();
+        while (i < izq.size() && j < der.size()) {
+            if (izq.get(i).getCantidad() <= der.get(j).getCantidad()) {
+                resultado.add(izq.get(i));
+                i++;
+            } else {
+                resultado.add(der.get(j));
+                j++;
+            }
+        }
+        while (i < izq.size()) {
+            resultado.add(izq.get(i));
+            i++;
+        }
+
+        while (j < der.size()) {
+            resultado.add(der.get(j));
+            j++;
+        }
+
+        return resultado;
+    }
+}
