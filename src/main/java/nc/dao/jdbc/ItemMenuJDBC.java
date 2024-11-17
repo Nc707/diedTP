dea29b9ab83b9b1855f982e00cea505d016dbd55
lucia <lucia@192.168.74.1>
2024-11-16 22:34:01 -0300

    se crea el test de PedidosController

--- src/main/java/nc/dao/jdbc/ItemMenuJDBC.java
+++ src/main/java/nc/dao/jdbc/ItemMenuJDBC.java
@@ -330,35 +330,23 @@ public class ItemMenuJDBC implements ItemMenuDAO {
                         + " WHERE im.id_vendedor = ?;";
                 break;
             case ID:
-                //query += "id = ?";
+
                 break;
             case CATEGORIA:
-                // DE LA TABLA TIENE_CATEGORIA
+               
                 break;
             case CATEGORIA_EXCLUYENTE:
-                //IDK
+              
                 break;
             case CATEGORIAS:
-                //DE LA TABLA CATEGORIA categoria IN (?)
+              
 
                 break;
             case PRECIO_TOPE:
-//                query = "SELECT im.id, im.nombre, im.descripcion, im.precio, im.id_vendedor, im.es_bebida,"
-//                        + "p.calorias, p.apto_celiaco, p.apto_vegano, p.peso,"
-//                        + "b.graduacion_alcoholica, b.tamaño AS tamaño_bebida, b.peso AS peso_bebida"
-//                        + "FROM item_menu im"
-//                        + "LEFT JOIN plato p ON p.id = im.id AND im.es_bebida = FALSE"
-//                        + "LEFT JOIN bebida b ON b.id = im.id AND im.es_bebida = TRUE"
-//                        + "WHERE im.precio <= ?;";
+
                 break;
             case PRECIO_MINIMO:
-//                query = "SELECT im.id, im.nombre, im.descripcion, im.precio, im.id_vendedor, im.es_bebida,"
-//                        + "p.calorias, p.apto_celiaco, p.apto_vegano, p.peso,"
-//                        + "b.graduacion_alcoholica, b.tamaño AS tamaño_bebida, b.peso AS peso_bebida"
-//                        + "FROM item_menu im"
-//                        + "LEFT JOIN plato p ON p.id = im.id AND im.es_bebida = FALSE"
-//                        + "LEFT JOIN bebida b ON b.id = im.id AND im.es_bebida = TRUE"
-//                        + "WHERE im.precio >= ?;";
+            
                 break;
         }
         try (PreparedStatement ps = conn.prepareStatement(query)) {
